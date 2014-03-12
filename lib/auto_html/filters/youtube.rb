912750e09288bce7b6b54f4aea018775ae8b9f43
Sergio Lopez M. <slopez@happypandacorp.com>
2014-03-12 13:08:10 -0600

    Gives the option to hide title on youtube videos

--- lib/auto_html/filters/youtube.rb
+++ lib/auto_html/filters/youtube.rb
@@ -1,4 +1,4 @@
-AutoHtml.add_filter(:youtube).with(:width => 420, :height => 315, :frameborder => 0, :wmode => nil, :autoplay => false, :hide_related => false) do |text, options|
+AutoHtml.add_filter(:youtube).with(:width => 420, :height => 315, :frameborder => 0, :wmode => nil, :autoplay => false, :hide_related => false, :showinfo => 0) do |text, options|
   regex = /https?:\/\/(www.)?(youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/watch\?feature=player_embedded&v=)([A-Za-z0-9_-]*)(\&\S+)?(\?\S+)?/
   text.gsub(regex) do
     youtube_id = $3
@@ -8,12 +8,13 @@ AutoHtml.add_filter(:youtube).with(:width => 420, :height => 315, :frameborder =
 		wmode = options[:wmode]
     autoplay = options[:autoplay]
     hide_related = options[:hide_related]
+    showinfo = options[:showinfo]
 		src = "//www.youtube.com/embed/#{youtube_id}"
     params = []
 		params << "wmode=#{wmode}" if wmode
     params << "autoplay=1" if autoplay
     params << "rel=0" if hide_related
     src += "?#{params.join '&'}" unless params.empty?
-    %{<iframe width="#{width}" height="#{height}" src="#{src}" frameborder="#{frameborder}" allowfullscreen></iframe>}
+    %{<iframe width="#{width}" height="#{height}" src="#{src}?showinfo=#{showinfo}" frameborder="#{frameborder}" allowfullscreen></iframe>}
   end
 end
